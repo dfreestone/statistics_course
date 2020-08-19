7be85f3179aeaaa9cfa5e9c364b7131b1a153000
David M. Freestone <freestoned@wpunj.edu>
2020-08-19 16:18:19 -0400

    Added pre_ and post_ GAD7 scores to yoga_anxiety

--- R/create_yoga_example.R
+++ R/create_yoga_example.R
@@ -16,14 +16,20 @@ b0 <- 18
 b1 <- -3
 sigma <- 2
 
-y <- b0 + b1*(condition == "yoga")
-y <- rnorm(n, y, sigma)
-y <- round(y)
+mu_pre <- b0
+mu_post <- b0 + b1*(condition == "yoga")
+
+y_pre <- rnorm(n, mu_pre, sigma)
+y_post <- rnorm(n, mu_post, sigma)
+
+y_pre <- round(y_pre)
+y_post <- round(y_post)
 
 data <- tibble(condition = condition,
-               GAD7 = y)
+               pre_GAD7 = y_pre,
+               post_GAD7 = y_post)
 
-fit <- lm(GAD7 ~ 0 + condition,
+fit <- lm(pre_GAD7 ~ 0 + condition,
           data = data)
 summary(fit)
 
